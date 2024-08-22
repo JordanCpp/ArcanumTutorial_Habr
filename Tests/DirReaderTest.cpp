1f79d17845b15d0b10b057e230fa7abf05d71ef5
Evgeniy <61433942+JordanCpp@users.noreply.github.com>
2024-08-22 10:06:35 +0400

    Add test for DirReader

--- Tests/DirReaderTest.cpp
+++ Tests/DirReaderTest.cpp
@@ -1,5 +1,6 @@
 #include <Pollux/TestEqual.hpp>
 #include <Pollux/Common/DirReader.hpp>
+#include <vector>
 
 using namespace Pollux;
 
@@ -7,14 +8,23 @@ int main()
 {
 	DirReader dirReader;
 
+	std::vector<std::string> list;
+
 	if (dirReader.Reset("TestFiles/" + dirReader.All()))
 	{
 		DirItem dirItem;
 
 		while (dirReader.Next(dirItem))
 		{
+			list.push_back(dirItem.Path);
 		}
 	}
+
+	std::vector<std::string>::iterator i = std::find(list.begin(), list.end(), "data");
+	POLLUX_TEST(i != list.end());
+
+	std::vector<std::string>::iterator j = std::find(list.begin(), list.end(), "arcanum4.dat");
+	POLLUX_TEST(j != list.end());
 
 	return 0;
 }
